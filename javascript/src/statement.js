316e4eafebbf579f62d096b7d6457d819daaf251
Pavel Kutáč <pavel.kutac@gmail.com>
2021-11-25 18:07:32 +0100

    Separate to function generating string output

--- javascript/src/statement.js
+++ javascript/src/statement.js
@@ -1,8 +1,20 @@
 
+function toString(result) {
+    let str = `Statement for ${result.customer}\n`;
+    for (let i = 0; i < result.plays.length; i++) {
+        str += ` ${result.plays[i].playName}: ${result.plays[i].eventCost} (${result.plays[i].seats} seats)\n`;
+    }
+    str += `Amount owed is ${result.amount}\n`;
+    str += `You earned ${result.credits} credits\n`;
+
+    return str;
+}
+
 function statement (invoice, plays) {
     let totalAmount = 0;
     let volumeCredits = 0;
-    let result = `Statement for ${invoice.customer}\n`;
+    // let result = `Statement for ${invoice.customer}\n`;
+    let result = {customer: invoice.customer, plays: []};
     const format = new Intl.NumberFormat("en-US",
         { style: "currency", currency: "USD",
             minimumFractionDigits: 2 }).format;
@@ -15,12 +27,24 @@ function statement (invoice, plays) {
         // add extra credit for every ten comedy attendees
         if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
         // print line for this order
-        result += ` ${play.name}: ${format(thisAmount/100)} (${perf.audience} seats)\n`;
+        // result += ` ${play.name}: ${format(thisAmount/100)} (${perf.audience} seats)\n`;
+
+        result.plays.push({
+            playName: play.name,
+            eventCost: format(thisAmount/100),
+            seats: perf.audience,
+        })
+
         totalAmount += thisAmount;
     }
-    result += `Amount owed is ${format(totalAmount/100)}\n`;
-    result += `You earned ${volumeCredits} credits\n`;
-    return result;
+    // result += `Amount owed is ${format(totalAmount/100)}\n`;
+    result.amount = format(totalAmount/100);
+    // result += `You earned ${volumeCredits} credits\n`;
+    result.credits = volumeCredits;
+
+    console.log(result)
+
+    return toString(result);
 }
 
 function getEventCost(playType, totalAudience) {
